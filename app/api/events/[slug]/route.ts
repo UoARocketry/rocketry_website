564ba374d06645b6734b1677a6441cd0e5965989
Puthikunkim <109781292+Puthikunkim@users.noreply.github.com>
2025-09-06 15:33:10 +1200

    More errrrrroorrsss

--- app/api/events/[slug]/route.ts
+++ app/api/events/[slug]/route.ts
@@ -1,14 +1,22 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import prisma from '../../../../lib/prisma';
 
-export async function GET( request: Request, { params }: { params: Record<string, string | string[]> }) {
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { slug: string | string[] } }
+) {
   const slug = Array.isArray(params.slug) ? params.slug[0] : params.slug;
   try {
     const event = await prisma.event.findUnique({ where: { slug } });
-    if (!event) return NextResponse.json({ error: 'Not found' }, { status: 404 });
+    if (!event) {
+      return NextResponse.json({ error: 'Not found' }, { status: 404 });
+    }
     return NextResponse.json(event);
   } catch (error) {
     console.error('Error fetching event by slug:', error);
-    return new Response(JSON.stringify({ slug }), { status: 200, headers: { "content-type": "application/json" },});
+    return NextResponse.json(
+      { error: 'Internal Server Error' },
+      { status: 500 }
+    );
   }
 }
